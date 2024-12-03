246dbdcb9786b7559dfc94c3a4af382bc6eef8a2
AWC2124R <cadenhwang05@gmail.com>
2024-12-02 23:07:16 -0500

    changed metadata

--- src/app/resume/layout.tsx
+++ src/app/resume/layout.tsx
@@ -1,9 +1,9 @@
 // src/app/layout.tsx
-import '../styles/globals.css'
+import '../../styles/globals.css'
 
 export const metadata = {
-  title: 'Taehoon Hwang | Main Page',
-  description: 'Portfolio of Taehoon Hwang',
+  title: 'Taehoon Hwang | Resume',
+  description: 'Resume of Taehoon Hwang',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
