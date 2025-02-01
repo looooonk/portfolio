cddf401e467ae0bb4019df24b2f3dcfd4c6d5e17
looooonk <cadenhwang05@gmail.com>
2025-01-31 19:13:08 -0500

    content: added award content

--- src/app/resume/components/awards.tsx
+++ src/app/resume/components/awards.tsx
@@ -1,36 +1,66 @@
 export default function Awards() {
     return (
         <div>
-            <div className="space-y-4 pt-4">
-                <div className="flex justify-between items-center space-x-2">
-                    <p className="font-bold">Purdue Office of Undergraduate Research Grant</p>
-                    <div className="h-0.5 flex-grow bg-black"></div>
-                    <p className="text-right">Purdue University, 2024</p>
+            <div className="space-y-8 pt-4">
+                <div className="space-y-1">
+                    <div className="flex justify-between items-center space-x-2">
+                        <p className="font-bold">Purdue Office of Undergraduate Research Grant</p>
+                        <div className="h-0.5 flex-grow bg-black"></div>
+                        <p className="text-right">Purdue University, 2024</p>
+                    </div>
+                    <p>
+                        Received $500 from the Office of Undergraduate Research based on project <span className="italic">WUMT: Wavelet U-Net Motion Transformer</span>.
+                    </p>
                 </div>
-                <div className="flex justify-between items-center space-x-2">
-                    <p className="font-bold">Dean&apos;s List & Semester Honors</p>
-                    <div className="h-0.5 flex-grow bg-black"></div>
-                    <p className="text-right">Purdue University, 2023 & 2024</p>
+                <div className="space-y-1">
+                    <div className="flex justify-between items-center space-x-2">
+                        <p className="font-bold">Dean&apos;s List & Semester Honors</p>
+                        <div className="h-0.5 flex-grow bg-black"></div>
+                        <p className="text-right">Purdue University, 2023 & 2024</p>
+                    </div>
+                    <p>
+                        Recieved both Dean's List and Semester Honors designations in Fall 2023, Spring 2024, and Fall 2024.
+                    </p>
                 </div>
-                <div className="flex justify-between items-center space-x-2">
-                    <p className="font-bold">Excellence Award in the Gifted School Research Conference</p>
-                    <div className="h-0.5 flex-grow bg-black"></div>
-                    <p className="text-right">Pohang University of Science & Technology, 2022</p>
+                <div className="space-y-1">
+                    <div className="flex justify-between items-center space-x-2">
+                        <p className="font-bold">Excellence Award in the Gifted School Research Conference</p>
+                        <div className="h-0.5 flex-grow bg-black"></div>
+                        <p className="text-right">Pohang University of Science & Technology, 2022</p>
+                    </div>
+                    <p>
+                        Recieved conference award for presentation done on open set recognition in ResNet models.
+                    </p>
                 </div>
-                <div className="flex justify-between items-center space-x-2">
-                    <p className="font-bold">Top Award for R&E Research in Computer Science</p>
-                    <div className="h-0.5 flex-grow bg-black"></div>
-                    <p className="text-right">Seoul Science High School, 2022</p>
+                <div className="space-y-1">
+                    <div className="flex justify-between items-center space-x-2">
+                        <p className="font-bold">Top Award for R&E Research in Computer Science</p>
+                        <div className="h-0.5 flex-grow bg-black"></div>
+                        <p className="text-right">Seoul Science High School, 2022</p>
+                    </div>
+                    <p>
+                        Recieved research award for improvement of open set recognition accuracy in ResNet models at Sungkyunkwan University.
+                    </p>
                 </div>
-                <div className="flex justify-between items-center space-x-2">
-                    <p className="font-bold">Azure AI Fundamentals Certification</p>
-                    <div className="h-0.5 flex-grow bg-black"></div>
-                    <p className="text-right">Microsoft, 2022</p>
+                <div className="space-y-1">
+                    <div className="flex justify-between items-center space-x-2">
+                        <p className="font-bold">Azure AI Fundamentals Certification</p>
+                        <div className="h-0.5 flex-grow bg-black"></div>
+                        <p className="text-right">Microsoft, 2022</p>
+                    </div>
+                    <p>
+                        Recieved AZ-900 certification from Microsoft on artificial intelligence & Microsoft Azure fundamentals.
+                    </p>
                 </div>
-                <div className="flex justify-between items-center space-x-2">
-                    <p className="font-bold">Top Award for Independent Research on Computer Science</p>
-                    <div className="h-0.5 flex-grow bg-black"></div>
-                    <p className="text-right">Seoul Science High School, 2021</p>
+                <div className="space-y-1">
+                    <div className="flex justify-between items-center space-x-2">
+                        <p className="font-bold">Top Award for Independent Research on Computer Science</p>
+                        <div className="h-0.5 flex-grow bg-black"></div>
+                        <p className="text-right">Seoul Science High School, 2021</p>
+                    </div>
+                    <p>
+                        Recieved research award for work on advertisement classification with OCR and semantic tree matching.
+                    </p>
                 </div>
             </div>
         </div>
